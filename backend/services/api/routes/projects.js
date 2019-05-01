54b96dbc366a6807a05945999938796bf64a0426
Ethan <ethan.lindeman@gmail.com>
2019-05-01 19:40:28 +0200

    Added Project creation
    Added project creation to projects/create route

--- backend/services/api/routes/projects.js
+++ backend/services/api/routes/projects.js
@@ -1,20 +1,52 @@
 const express = require('express');
 const router = express.Router();
 
+const mongoose = require('mongoose');
+const Project = require('../models/project');
+
+
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: "list of projects"
+    Project.find()
+    .exec()
+    .then(results => {
+      console.log(results);
+      res.status(200).json(results);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
     });
 });
 
 router.post('/create', (req, res, next) => {
-    const project = {
+    const project = new Project({
+        _id: new mongoose.Types.ObjectId(),
         project_name: req.body.name,
-        project_results: req.body.result
-    }
-    res.status(200).json({
-        message: "create project",
-        project: project
+        whitelist: req.body.whitelist,
+        blacklist: req.body.blacklist,
+        source: req.body.source,
+        startTime: req.body.startTime,
+        trackTime: req.body.trackTime,
+        data: null,
+        createdBy: "Test User"
+    });
+
+    project
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Handling POST requests to /projects/create",
+        createdProduct: result
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
     });
 });
 
