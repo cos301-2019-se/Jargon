dd4007e5e41f8e93394cdf95ba4699af5c3e529e
KevinCoetzee10 <kevincoetzee@protonmail.com>
2019-05-02 09:58:53 +0200

    added password hashing

--- backend/services/api/routes/login.js
+++ backend/services/api/routes/login.js
@@ -2,13 +2,15 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../models/user');
+const bcrypt = require('bcrypt-nodejs');
+
 
 router.post('/', (req, res, next) => {
     User.find({
         email : req.body.email
     }).exec()
     .then(results => {
-        if(results[0].password===req.body.password){
+        if( bcrypt.compareSync(req.body.password, results[0].password)){
             res.status(200).json({
                 authenticated: true
             });
--- backend/services/api/routes/register.js
+++ backend/services/api/routes/register.js
@@ -2,15 +2,18 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../models/user');
+const bcrypt = require('bcrypt-nodejs');
 
 router.post('/', (req, res, next) => {
+    let saltToSave = bcrypt.genSaltSync();
+    let passwordToSave = bcrypt.hashSync(req.body.password, saltToSave);
     const user = new User({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         surname: req.body.surname,
         email: req.body.email,
         username: req.body.username,
-        password: req.body.password,
+        password: passwordToSave,
         admin: req.body.admin,
         projects: null
     });
