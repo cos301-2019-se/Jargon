b5569c890c38b2fde2acc7b7000025a0698d83ea
KevinCoetzee10 <kevincoetzee@protonmail.com>
2019-05-20 17:46:21 +0200

    added blacklisting feature to twitterlistener

--- backend/services/listeners/platforms/twitterListener.js
+++ backend/services/listeners/platforms/twitterListener.js
@@ -1,24 +1,32 @@
+"use strict";
+
 class twitterListener{
     constructor(whitelist_, blacklist_, trackingDuration_){
         this.whitelist = "";
         this.blacklist = "";
+        this.blacklistArray = blacklist_;
+        this.whitelistArray = whitelist_;
         this.trackingDuration = trackingDuration_;
         for(var x = 0; x < whitelist_.length; x++){
             if(x==0){
                 this.whitelist = whitelist_[x];
             }else{
-                this.whitelist = "," + whitelist_[x];
+                this.whitelist += "," + whitelist_[x];
             }
         }
         for(var x = 0; x < blacklist_.length; x++){
             if(x==0){
                 this.blacklist = blacklist_[x];
             }else{
-                this.blacklist = "," + blacklist_[x];
+                this.blacklist += "," + blacklist_[x];
             }
         }
     }
    
+    get blacklistArray(){
+        return this._blacklistArray;
+    }
+
     get blacklist(){
         return this._blacklist;
     }
@@ -29,6 +37,10 @@ class twitterListener{
 
     get trackingDuration(){
         return this._trackingDuration;
+    }
+
+    set blacklistArray(vals){
+        this._blacklistArray = vals;
     }
 
     set blacklist(vals){
@@ -55,8 +67,16 @@ class twitterListener{
         })
         let tempArray = [];
         t.on('tweet', function(tweet){
-            tempArray.push(tweet);
-        })
+            let found = false;
+            this.blacklistArray.forEach(element=>{
+                if(tweet.text.indexOf(element)>-1){
+                    found = true;
+                }
+            })
+            if(!found){
+                tempArray.push(tweet);
+            }
+        }.bind(this))
         t.track(this.whitelist); 
         setTimeout(()=>{
             t.untrack(this.whitelist);
@@ -78,7 +98,7 @@ class twitterListener{
             token: '1122433281465700352-bPVkrTzBiwMyenSqHfePpi2QNU4t3e',
             token_secret: twitterTokenSecret
         })
-        this.whitelist.forEach(element => {
+        this.whitelistArray.forEach(element => {
             t.untrack(element); 
         }); 
     }
