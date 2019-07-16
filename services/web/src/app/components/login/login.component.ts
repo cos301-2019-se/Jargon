e364edd1632de9aebc4c0b64b660a9d1563eace9
Ethan <ethan.lindeman@gmail.com>
2019-07-16 21:06:45 +0200

    Added authentication check

--- services/web/src/app/components/login/login.component.ts
+++ services/web/src/app/components/login/login.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 import { LoginApiRequesterService } from '../../services/login-api-requester/login-api-requester.service';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent implements OnInit {
 
-  constructor(private loginApiRequester : LoginApiRequesterService) { }
+  
+
+  constructor(private router : Router, private loginApiRequester : LoginApiRequesterService) { }
 
   ngOnInit() {
   }
@@ -20,8 +24,19 @@ export class LoginComponent implements OnInit {
     const email = values.email;
     const pass = values.password;
 
+    console.log(values);
+    console.log(`${email}   ${pass}`);
+
     this.loginApiRequester.authenticateUser(email, pass).subscribe((res : any) => {
       console.log(res);
+      if (res.authenticated)
+      {
+        //router here
+      }
+      else
+      {
+
+      }
     });
   }
 
