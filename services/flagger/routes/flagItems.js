de5d0f993060787610fcce186a7df462e8ed5840
Ethan <ethan.lindeman@gmail.com>
2019-07-18 17:06:39 +0200

    Removed erroneous import

--- services/flagger/routes/flagItems.js
+++ services/flagger/routes/flagItems.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const FlaggedItem = require('../models/flaggedItem');
 const Epoch = require('../models/epoch');
 
 const cap = 10;
