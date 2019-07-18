f387d49c8c8c9702f442a07e62c286c50dac3b05
sarrost <hmagaya15@gmail.com>
2019-07-18 22:09:52 +0200

    Update test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,7 +1,19 @@
+#! /bin/sh
+# This shell script executes each services unit test procedure.
+
 cd services
 status=0
-mocha controller/test.js --exit && status+=$(( $? ))
-mocha listeners/test.js --exit && status +=$(( $? ))
-mocha cleaners/test.js --exit && status +=$(( $? ))
+
+# controller service
+mocha controller/test.js --exit 
+status+=$(( $? ))
+
+# flagger service
+mocha flagger/test.js --timeout 15000
+status+=$(( $? ))
+
+#mocha listeners/test.js --exit && status +=$(( $? ))
+#mocha cleaners/test.js --exit && status +=$(( $? ))
+
 cd ..
 exit $status
