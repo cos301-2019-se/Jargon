13359d816f4dfbe76402c5a487c0dd2d8233b27c
sarrost <hmagaya15@gmail.com>
2019-07-18 17:52:51 +0200

    Update test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,7 +1,7 @@
 cd services
-status = 0
-mocha controller/test.js --exit && status += $?
-mocha listeners/test.js --exit && status += $?
-mocha cleaners/test.js --exit && status += $?
+status=0
+mocha controller/test.js --exit && status+=$(( $? ))
+mocha listeners/test.js --exit && status +=$(( $? ))
+mocha cleaners/test.js --exit && status +=$(( $? ))
 cd ..
 exit $status
