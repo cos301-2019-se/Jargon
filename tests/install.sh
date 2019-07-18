0abe2c16b8d147ab62f91aac5ee8779245caeefe
sarrost <hmagaya15@gmail.com>
2019-07-18 15:35:28 +0200

    Update install.sh

--- tests/install.sh
+++ tests/install.sh
@@ -6,11 +6,8 @@ npm install chai chai-http chai-arrays
 # install hidden files
 cd ../services/listeners/db
 wget --no-check-certificate "$DBCONFIGLINK" -O dbconfig.js
-ls
 cd ../platforms
-wget --no-check-certificate "$TWITTERLISTENERLINK" -O twitterListener.js
+wget --no-check-certificate "$TWITTERLISTENERLINK" -O twitterConfig.js
 cd ../../controller/db
-ls
 wget --no-check-certificate "$DBCONFIGLINK" -O dbconfig.js
 cd ../../../tests
-ls
