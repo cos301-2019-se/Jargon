47429c3e79bb20de02ca4ce30dc833131b0ba9bf
KevinCoetzee10 <kevincoetzee@protonmail.com>
2019-08-19 13:10:48 +0200

    cleaned listener routing pages

--- services/listeners/server.js
+++ services/listeners/server.js
@@ -1,13 +1,17 @@
+/**
+ * Filename: app.js
+ * Author: Kevin Coetzee
+ * 
+ *      This file provides the API routing for the
+ *      different files within the listeners microservice
+ */
 "use strict";
 
 const http = require("http");
 const app = require('./app');
 const port = process.env.PORT || 3001;
 
-
 const server = http.createServer(app);
-
 server.listen(port);
-
 const db = require('./db/db');
 db();
