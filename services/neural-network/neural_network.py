a4621a2da2d776ab3c3f6ea13174e192bbdb23ec
sarrost <hmagaya15@gmail.com>
2019-08-20 18:21:11 +0200

    Update NN

--- services/neural-network/neural_network.py
+++ services/neural-network/neural_network.py
@@ -2,18 +2,17 @@
     Filename: neural-network.py
     Author  : Christiaan Nel
     Type    : Class
-    
+
         The neural-network.py contains the NeuralNetwork class.
-        
+
 """
 import os
-import random
+import time
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from datetime import datetime
 from torchtext import data
-from torchtext import datasets
+import torch.optim as optim
 
 
 class NeuralNetwork(nn.Module):
@@ -23,30 +22,32 @@ class NeuralNetwork(nn.Module):
 
         Uses a pre-trained English word embedding neaural net.
     """
-    def __init__(self, vocab_size, embedding_dim, n_filters, filter_sizes, 
-                output_dim, dropout, pad_idx):
+    def __init__(self, train_data, valid_data, test_data, vocab_size,
+                 embedding_dim, n_filters, filter_sizes, output_dim,
+                 dropout, pad_idx):
         """
-            __init__(self, vocab_size, embedding_dim, n_filters, filter_sizes, 
+            __init__(self, vocab_size, embedding_dim, n_filters, filter_sizes,
                 output_dim, dropout, pad_idx) : none
             Contstructor.
         """
 
         super().__init__()
 
-        self.device = torch.device('cuda' if torch.cuda.is_available() 
-                else 'cpu')
+        self.device = torch.device(
+                'cuda' if torch.cuda.is_available() else 'cpu')
         self.device = torch.device('cpu')
         print('Using device: ', self.device)
 
-        self.embedding = nn.Embedding(vocab_size, embedding_dim, 
-                    padding_idx = pad_idx)
+        self.embedding = nn.Embedding(
+                vocab_size, embedding_dim, padding_idx=pad_idx)
 
         self.convs = nn.ModuleList([
-                    nn.Conv2d(in_channels = 1,
-                    out_channels = n_filters,
-                    kernel_size = (fs, embedding_dim))
-                    for fs in filter_sizes
-                ])
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=n_filters,
+                kernel_size=(fs, embedding_dim)
+            ) for fs in filter_sizes
+        ])
 
         self.fc = nn.Linear(len(filter_sizes) * n_filters, output_dim)
 
@@ -54,6 +55,16 @@ class NeuralNetwork(nn.Module):
 
         if self.load():
             print('Saved model state init')
+        else:
+            BATCH_SIZE = 64
+            train_iterator, valid_iterator, test_iterator = \
+                data.BucketIterator.splits(
+                    (train_data, valid_data, test_data),
+                    sort=False,  # don't sort test/validation data
+                    batch_size=BATCH_SIZE,
+                    device=self.device)
+
+            train_model(self, train_iterator, valid_iterator)
 
     def save(self):
         """
@@ -69,12 +80,15 @@ class NeuralNetwork(nn.Module):
             If no file containing a model is found, the return value is False.
         """
         if os.path.isfile('./nn-model.pt'):
-            self.load_state_dict(torch.load('./nn-model.pt',
-                    map_location=self.device))
+            self.load_state_dict(
+                torch.load(
+                    './nn-model.pt',
+                    map_location=self.device
+                )
+            )
             self.eval()
             return True
         return False
-
 
     def forward(self, text):
         """
@@ -85,14 +99,16 @@ class NeuralNetwork(nn.Module):
         embedded = self.embedding(text)
         embedded = embedded.unsqueeze(1)
         conved = [F.relu(conv(embedded)).squeeze(3) for conv in self.convs]
-        pooled = [F.max_pool1d(conv, conv.shape[2]).squeeze(2) for conv in conved]
-        cat = self.dropout(torch.cat(pooled, dim = 1))
+        pooled = [
+            F.max_pool1d(conv, conv.shape[2]).squeeze(2) for conv in conved
+        ]
+        cat = self.dropout(torch.cat(pooled, dim=1))
 
         return self.fc(cat)
 
     def evaluate(self, indexed):
         """
-            evaluate(self, indexed) : 
+            evaluate(self, indexed) :
             Description of the function.
         """
         tensor = torch.LongTensor(indexed).to(self.device)
@@ -101,3 +117,119 @@ class NeuralNetwork(nn.Module):
 
         return prediction.item()
 
+
+def epoch_time(start_time, end_time):
+    elapsed_time = end_time - start_time
+    elapsed_mins = int(elapsed_time / 60)
+    elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+    return elapsed_mins, elapsed_secs
+
+
+def evaluate(model, iterator, criterion):
+
+    epoch_loss = 0
+    epoch_acc = 0
+
+    model.eval()
+
+    with torch.no_grad():
+
+        for batch in iterator:
+
+            predictions = model(batch.t).squeeze(1)
+
+            loss = criterion(predictions, torch.squeeze(batch.s))
+            acc = binary_accuracy(predictions, torch.squeeze(batch.s))
+
+            epoch_loss += loss.item()
+            epoch_acc += acc.item()
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
+
+
+def train_model(model, train_iterator, valid_iterator):
+    """TODO: Docstring for train_model.
+    :returns: TODO
+
+    """
+
+    optimizer = optim.Adam(model.parameters())
+
+    criterion = nn.BCEWithLogitsLoss()
+
+    model = model.to(model.device)
+    criterion = criterion.to(model.device)
+
+    N_EPOCHS = 5
+
+    best_valid_loss = float('inf')
+
+    for epoch in range(N_EPOCHS):
+
+        start_time = time.time()
+
+        train_loss, train_acc = \
+            train(model, train_iterator, optimizer, criterion)
+        valid_loss, valid_acc = evaluate(model, valid_iterator, criterion)
+
+        end_time = time.time()
+
+        epoch_mins, epoch_secs = epoch_time(start_time, end_time)
+
+        if valid_loss < best_valid_loss:
+            best_valid_loss = valid_loss
+            torch.save(model.state_dict(), 'nn-model.pt')
+
+        print(f'Epoch: {epoch+1:02} | Epoch Time: {epoch_mins}m {epoch_secs}s')
+        print(
+            f'\tTrain Loss: {train_loss:.3f} | Train Acc: {train_acc*100:.2f}%'
+        )
+        print(
+            f'\t Val. Loss: {valid_loss:.3f} |  Val. Acc: {valid_acc*100:.2f}%'
+        )
+
+
+def binary_accuracy(preds, y):
+    """
+    Returns accuracy per batch, i.e. if you get 8/10 right, this returns 0.8,
+    NOT 8
+    """
+
+    # round predictions to the closest integer
+    rounded_preds = torch.round(torch.sigmoid(preds))
+    correct = (rounded_preds == y).float()  # convert into float for division
+    acc = correct.sum() / len(correct)
+    return acc
+
+
+def train(model, iterator, optimizer, criterion):
+
+    epoch_loss = 0
+    epoch_acc = 0
+
+    model.train()
+    num = 0
+    batch_count = len(iterator)
+    prev_progress = -1
+
+    for batch in iterator:
+        optimizer.zero_grad()
+        predictions = model(batch.t).squeeze(1)
+        loss = criterion(predictions, torch.squeeze(batch.s))
+        acc = binary_accuracy(predictions, torch.squeeze(batch.s))
+
+        loss.backward()
+        optimizer.step()
+
+        epoch_loss += loss.item()
+        epoch_acc += acc.item()
+
+        progress = int(round((num / batch_count) * 100))
+        if (progress > prev_progress):
+            print('-> [' + str(
+                progress) + '%] Batch: ' + str(num) + '/' + str(batch_count)
+            )
+        num += 1
+        prev_progress = progress
+
+    return epoch_loss / len(iterator), epoch_acc / len(iterator)
--- services/neural-network/operations.py
+++ services/neural-network/operations.py
@@ -2,19 +2,16 @@
     Filename: operations.py
     Author  : Christiaan Nel
     Type    : Functions
-    
+
         The operations.py file contains the methods for configuring
         the neural network. It also contains API methods for
         interfacing with the NeuralNetwork class.
 """
-import json
 import neural_network as nno
-import random
 import spacy
 import torch
 from torchtext import data
-from torchtext import datasets
-
+from math import floor
 
 SEED = 1234
 
@@ -22,66 +19,110 @@ torch.manual_seed(SEED)
 torch.backends.cudnn.deterministic = True
 
 MAX_VOCAB_SIZE = 25_000
-TEXT = data.Field(tokenize = 'spacy')
-LABEL = data.LabelField(dtype = torch.float)
 
-print('Receiving data...')
-train_data, test_data = datasets.IMDB.splits(TEXT, LABEL)
-train_data, valid_data = train_data.split(random_state = random.seed(SEED))
+print('-> Receiving data...')
+SENTIMENT = data.Field()
+TWEET = data.Field(tokenize='spacy')
 
-print('Building vocab...')
-TEXT.build_vocab(train_data,
-        max_size = MAX_VOCAB_SIZE,
-        vectors = 'glove.6B.100d',
-        unk_init = torch.Tensor.normal_)
+fields = [
+    ('s', SENTIMENT),
+    (None, None),
+    (None, None),
+    (None, None),
+    (None, None),
+    ('t', TWEET)
+]
 
-INPUT_DIM = len(TEXT.vocab)
+train_data = data.TabularDataset(
+    path='data/train.csv',
+    format='csv',
+    fields=fields,
+    skip_header=True
+)
+train_count = len(train_data)
+print('-> Dataset contains ' + str(train_count) + ' data points.')
+
+print('-> Building vocab...')
+SENTIMENT.build_vocab(train_data)
+TWEET.build_vocab(
+    train_data,
+    max_size=MAX_VOCAB_SIZE,
+    vectors='glove.6B.100d',
+    unk_init=torch.Tensor.normal_
+)
+
+print('-> Splitting dataset...')
+test_count = floor(0.1 * train_count)
+valid_count = test_count
+train_count = train_count - test_count
+train_data, test_data = train_data.split([test_count, train_count])
+train_count = train_count - valid_count
+train_data, valid_data = train_data.split([valid_count, train_count])
+
+print('-> Dataset split into training ('+str(train_count)+') '
+      'and test ('+str(test_count)+') and validation ('+str(valid_count)+').')
+print(vars(train_data[0]))
+
+INPUT_DIM = len(TWEET.vocab)
 EMBEDDING_DIM = 100
 N_FILTERS = 100
 FILTER_SIZE = [3, 4, 5]
 OUTPUT_DIM = 1
 DROPOUT = 0.5
-PAD_IDX = TEXT.vocab.stoi[TEXT.pad_token]
+PAD_IDX = TWEET.vocab.stoi[TWEET.pad_token]
 
-cnn = nno.NeuralNetwork(INPUT_DIM, EMBEDDING_DIM, N_FILTERS, 
-        FILTER_SIZE, OUTPUT_DIM, DROPOUT, PAD_IDX)
+cnn = nno.NeuralNetwork(
+    train_data,
+    valid_data,
+    test_data,
+    INPUT_DIM,
+    EMBEDDING_DIM,
+    N_FILTERS,
+    FILTER_SIZE,
+    OUTPUT_DIM,
+    DROPOUT,
+    PAD_IDX
+)
 
-pretrained_embedding = TEXT.vocab.vectors
+pretrained_embedding = TWEET.vocab.vectors
 
 cnn.embedding.weight.data.copy_(pretrained_embedding)
 print("Pretrained embeddings applied")
 
 nlp = spacy.load('en')
 
-def create_indexed(sentence, min_len = 5):
+
+def create_indexed(sentence, min_len=5):
     """
-        create_indexed(sentence, min_len = 5): 
+        create_indexed(sentence, min_len = 5):
     """
     tokenized = [tok.text for tok in nlp.tokenizer(sentence)]
 
     if len(tokenized) < min_len:
         tokenized += ['<pad>'] * (min_len - len(tokenized))
 
-    return [TEXT.vocab.stoi[t] for t in tokenized]
+    return [TWEET.vocab.stoi[t] for t in tokenized]
 
 
 def evaluate(data):
     """
-        evaluate(data): 
+        evaluate(data):
         Evaulate a list of strings and return a sentiment for each.
     """
     print(data)
     indexed = [create_indexed(sentence) for sentence in data['data']]
-    results = [cnn.evaluate(indexed_sentence) for indexed_sentence
-            in indexed]#, 200
+    results = [
+        cnn.evaluate(indexed_sentence) for indexed_sentence in indexed
+    ]  # 200
     res = {}
     res['sentiments'] = results
     return res
 
+
 def train(data):
     """
         train(data):
-        Trains the neural network. 
+        Trains the neural network.
     """
     print(data)
     return cnn.train('')
