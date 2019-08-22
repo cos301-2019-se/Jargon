10f63bcb37a0d303e0262dada7052770d5689cd5
Graeme Coetzee <graemecoetzee123@gmail.com>
2019-08-23 01:18:22 +0200

    Fix Charts

--- services/web/src/app/components/dashboard/dashboard.module.ts
+++ services/web/src/app/components/dashboard/dashboard.module.ts
@@ -11,7 +11,7 @@ import { RefreshGuardService } from '../../services/refresh-guard/refresh-guard.
 import { ProjectApiRequesterService } from '../../services/project-api-requester/project-api-requester.service';
 import { NeuralnetApiRequesterService } from '../../services/neuralnet-api-requester/neuralnet-api-requester.service';
 import { SharedProjectService } from '../../services/shared-project/shared-project.service';
-import { HistogramSeriesService, LineSeriesService, PieSeriesService, AccumulationDataLabelService, AccumulationAnnotationService, AccumulationTooltipService, AccumulationLegendService, AccumulationChartModule } from '@syncfusion/ej2-angular-charts';
+import { HistogramSeriesService, LineSeriesService, PieSeriesService, AccumulationDataLabelService, AccumulationAnnotationService, AccumulationTooltipService, AccumulationLegendService, AccumulationChartModule, DateTimeService } from '@syncfusion/ej2-angular-charts';
 import { ProjectCompareComponent } from './project-compare/project-compare.component';
 
 @NgModule({
