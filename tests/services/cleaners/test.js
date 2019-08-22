efef983f42e71e1b0961826101babe37e8a5c0c0
KevinCoetzee10 <kevincoetzee@protonmail.com>
2019-08-23 01:12:56 +0200

    added cleaners test

--- tests/services/cleaners/test.js
+++ tests/services/cleaners/test.js
@@ -9,26 +9,52 @@ chai.should();
 
 const host = "localhost:3003";
 
-describe('/twitter POST success', () => {
+describe('/cleaners', () => {
     const path = '/twitter';
 
-    it('passed POST', () => {
-       return chai
-        .request(host)
-        .post(path)
-        .send(
-            {
-                "rawData" : [ { 
-                "text" : "RT username: this is a tweet"
-                }]
-            }
-        )
-        .then((res) => {
-            chai.expect(res).to.have.status(200);  
-            // chai.expect(res.body.data).to.be.array();
-        }, (err) => {
-            chai.expect(err.response).to.have.status(500);
+    describe('Cleaning project - valid data, already clean', () => {
+        it('passed POST', () => {
+            return chai
+            .request(host)
+            .post(path)
+            .send(
+                {
+                    "rawData" : [ { 
+                    "text" : "RT username: this is a tweet"
+                    }]
+                }
+            )
+            .then((res) => {
+                chai.expect(res).to.have.status(200);  
+                // chai.expect(res.body.data).to.be.array();
+            }, (err) => {
+                chai.expect(err.response).to.have.status(500);
+            });
         });
-    });
-   
+    })
+});
+
+describe('/cleaners', () => {
+    const path = '/twitter';
+
+    describe('Cleaning project - valid data, already clean', () => {
+        it('passed POST', () => {
+            return chai
+            .request(host)
+            .post(path)
+            .send(
+                {
+                    "rawData" : [ { 
+                    "text" : "RT username: this is a tweet"
+                    }]
+                }
+            )
+            .then((res) => {
+                chai.expect(res).to.have.status(200);  
+                // chai.expect(res.body.data).to.be.array();
+            }, (err) => {
+                chai.expect(err.response).to.have.status(500);
+            });
+        });
+    })
 });
