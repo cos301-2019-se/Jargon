f52c9cd93e3fa93a94b82a95a636813c948b73e1
KevinCoetzee10 <kevincoetzee@protonmail.com>
2019-08-22 22:49:02 +0200

    added basic receive rmq functions

--- services/controller/services/api/routes/projects.js
+++ services/controller/services/api/routes/projects.js
@@ -11,7 +11,7 @@ const router = express.Router();
 
 const mongoose = require('mongoose');
 const Project = require('../models/project');
-var ampq = require('amqplib/callback_api');
+const amqp = require('amqplib/callback_api');
 var amqpConn = null;
 
 /***
@@ -471,48 +471,92 @@ router.post('/startStream', (req, res, next) => {
     })
 });
 
-function startRMQ() {
-    amqp.connect(process.env.CLOUDAMQP_URL + "?heartbeat=60", function(err, conn) {
-      if (err) {
-        console.error("[AMQP]", err.message);
-        return setTimeout(start, 1000);
-      }
-      conn.on("error", function(err) {
-        if (err.message !== "Connection closing") {
-          console.error("[AMQP] conn error", err.message);
+function startRMQ(){
+    var args = process.argv.slice(2);
+
+    // if (args.length == 0) {
+    // console.log("Usage: receive_logs_direct.js [info] [warning] [error]");
+    // process.exit(1);
+    // }
+
+    amqp.connect('amqp://localhost', function(error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function(error1, channel) {
+        if (error1) {
+        throw error1;
         }
-      });
-      conn.on("close", function() {
-        console.error("[AMQP] reconnecting");
-        return setTimeout(start, 1000);
-      });
-      console.log("[AMQP] connected");
-      amqpConn = conn;
-      whenConnected();
+        var exchange = 'direct_logs';
+
+        channel.assertExchange(exchange, 'direct', {
+        durable: false
+        });
+
+        channel.assertQueue('', {
+        exclusive: true
+        }, function(error2, q) {
+            if (error2) {
+            throw error2;
+            }
+        console.log(' [*] Waiting for logs. To exit press CTRL+C');
+
+        args.forEach(function(severity) {
+            channel.bindQueue(q.queue, exchange, severity);
+        });
+
+        channel.consume(q.queue, function(msg) {
+            console.log(" [x] %s: '%s'", msg.fields.routingKey, msg.content.toString());
+        }, {
+            noAck: true
+        });
+        });
     });
-  };
+    });
+}
 
-  function whenConnected() {
-    startWorker();
-  }
+// function startRMQ() {
+//     amqp.connect('amqp://localhost', function(err, conn) {
+//       if (err) {
+//         console.error("[AMQP]", err.message);
+//         return setTimeout(start, 1000);
+//       }
+//       conn.on("error", function(err) {
+//         if (err.message !== "Connection closing") {
+//           console.error("[AMQP] conn error", err.message);
+//         }
+//       });
+//       conn.on("close", function() {
+//         console.error("[AMQP] reconnecting");
+//         return setTimeout(start, 1000);
+//       });
+//       console.log("[AMQP] connected");
+//       amqpConn = conn;
+//       whenConnected();
+//     });
+//   };
 
-  function startWorker() {
-    amqpConn.createChannel(function(err, ch) {
-      if (closeOnErr(err)) return;
-      ch.on("error", function(err) {
-        console.error("[AMQP] channel error", err.message);
-      });
-      ch.on("close", function() {
-        console.log("[AMQP] channel closed");
-      });
+//   function whenConnected() {
+//     startWorker();
+//   }
+
+//   function startWorker() {
+//     amqpConn.createChannel(function(err, ch) {
+//       if (closeOnErr(err)) return;
+//       ch.on("error", function(err) {
+//         console.error("[AMQP] channel error", err.message);
+//       });
+//       ch.on("close", function() {
+//         console.log("[AMQP] channel closed");
+//       });
   
-      ch.prefetch(10);
-      ch.assertQueue("controller_queue", { durable: true }, function(err, _ok) {
-        if (closeOnErr(err)) return;
-        ch.consume("controller_queue", processMsg, { noAck: false });
-        console.log("Worker is started");
-      });
-    });
-  }
+//       ch.prefetch(10);
+//       ch.assertQueue("controller_queue", { durable: true }, function(err, _ok) {
+//         if (closeOnErr(err)) return;
+//         ch.consume("controller_queue", processMsg, { noAck: false });
+//         console.log("Worker is started");
+//       });
+//     });
+//   }
 
 module.exports = router;
