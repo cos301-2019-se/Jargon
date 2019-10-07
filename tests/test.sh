dbdb6059ef317451f61a9f5d96d7e5d639407e52
sarrost <herbert.magaya@protonmail.com>
2019-10-07 02:48:35 +0200

    Update documentation

--- tests/test.sh
+++ tests/test.sh
@@ -1,27 +1,40 @@
-#! /bin/sh
-# This shell script executes each services unit test procedure.
+#!/usr/bin/env bash
+#
+#    Filename: test.sh
+#    Author  : Herbert Magaya
+#    Type    : Script
+#
+#       This shell script executes each service's unit test procedure.
+##
 
+# start
 cd services
 status=0
 
 # cleaner service
+echo "->    Begin testing cleaner service."
 mocha cleaners/test.js --timeout 15000
-#status+=$?
+status=$(( $status + $? ))
+echo "->    Done testing cleaner service.\n\n"
 
 # controller service
+echo "->    Begin testing controller service."
 mocha controller/test.js --timeout 15000 --exit 
-#status+=$?
+status=$(( $status + $? ))
+echo "->    Done testing controller service.\n\n"
 
 # flagger service
+echo "->    Begin testing flagger service."
 mocha flagger/test.js --timeout 15000
-#status+=$?
+status=$(( $status + $? ))
+echo "->    Done testing flagger service.\n\n"
 
 # listener service
-#mocha listeners/test.js --timeout 150000
-#status+=$?
+echo "->    Begin testing listeners service."
+mocha listeners/test.js --timeout 150000
+status=$(( $status + $? ))
+echo "->    Done testing listeners service.\n\n"
 
-#mocha listeners/test.js --exit && status +=$(( $? ))
-#mocha cleaners/test.js --exit && status +=$(( $? ))
-
+# finish
 cd ..
 exit $status
