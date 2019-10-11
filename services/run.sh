8110ac86793da606e9906a0031c5433127c31b8c
sarrost <herbert.magaya@protonmail.com>
2019-10-11 10:00:09 +0200

    Update run.sh

--- services/run.sh
+++ services/run.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
-cd docker/compose
-docker-compose up &
-cd ../..
+# cd docker/compose
+# docker-compose up &
+# cd ../..
+cd analysis && npm start & 
+cd ../controller && npm start & 
+cd ../cleaners && npm start & 
+cd ../flagger && npm start & 
+cd ../listeners && npm start & 
+cd ../wbsocket && npm start & 
+cd ../neural-network && python3 receive.py & 
+cd .. && docker start compose_web_1 &
